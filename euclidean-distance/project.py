10c65daa488467ea7877080892bb0532f4287ea0
theEMA <61084889+theEMA-dev@users.noreply.github.com>
2024-08-12 13:20:48 +0300

    revamped python code

--- euclidean-distance/project.py
+++ euclidean-distance/project.py
@@ -1,19 +1,20 @@
 import math, sys, time
 print("\033[32m"+time.ctime())
 print(sys.version)
-print("Euclidean Distance Calculator by theEMA-dev")
-print("\033[37mEnter (x, y) coordinates of point1: 1,2")
-print("Enter (x, y) coordinates of point2: 4,6")
-points = [(1, 2), (4, 6)] # 3 4 5 Euclidean Distance
+
+points = [(1, 2), (4, 2), (4, 6)]
+
+print("\033[36mEuclidean Distance Calculator by theEMA-dev")
+print("Running Python code in PyScript 2024.8.2")
+print("\033[37mCoordinates:", points)
 
 def euclideanDistance(point1, point2):
-  return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
-distances = []
-for i in range(len(points)):
-  for j in range(i + 1, len(points)):
-    distance = euclideanDistance(points[i], points[j])
-    distances.append(distance)
+distance_1_2 = euclideanDistance(points[0], points[1])
+distance_1_3 = euclideanDistance(points[0], points[2])
+distance_2_3 = euclideanDistance(points[1], points[2])
 
-min_distance = min(distances)
-print(f"Minimum distance: {min_distance}")
+print(f"Distance between point 1 and point 2: {distance_1_2}")
+print(f"Distance between point 1 and point 3: {distance_1_3}")
+print(f"Distance between point 2 and point 3: {distance_2_3}")

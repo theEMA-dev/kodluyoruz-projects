ae4f46d6e25019abceb316b270da4ea56f5e00cd
theEMA <61084889+theEMA-dev@users.noreply.github.com>
2024-08-12 12:22:07 +0300

    commit

--- euclidean-distance/project.py
+++ euclidean-distance/project.py
@@ -1,5 +1,9 @@
-import math
-
+import math, sys, time
+print("\033[32m"+time.ctime())
+print(sys.version)
+print("Euclidean Distance Calculator by theEMA-dev")
+print("\033[37mEnter (x, y) coordinates of point1: 1,2")
+print("Enter (x, y) coordinates of point2: 4,6")
 points = [(1, 2), (4, 6)] # 3 4 5 Euclidean Distance
 
 def euclideanDistance(point1, point2):
